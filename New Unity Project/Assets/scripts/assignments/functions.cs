114cbb36f950b2c6f8697a11806db92d37e53bc3
Nathan <Notallthatevil@gmail.com>
2019-02-11 16:34:12 -0700

    started project unity

---
+++ New Unity Project/Assets/scripts/assignments/functions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class functions : MonoBehaviour {
+
+	public string weather;
+
+	void start(){
+		Weather(weather);
+	}
+
+	void  Weather(string weatherstate){
+
+		if(weatherstate == "sunny"){
+			print("the sun is shining today");
+		}
+		else if(weatherstate == "raining"){
+			print("it is soggy");
+		}
+			else if(weatherstate == "windy"){
+			print("it is windy");
+		}	
+		else if(weatherstate == "snowing"){
+			print("it is a blizzard out there");
+		}	
+		else if(weatherstate == "foggy"){
+			print("i can't see");
+		}
+			else{
+				print("i don't understand"+ weatherstate);
+			}
+
+	}
+}
---
+++ New Unity Project/Assets/scripts/assignments/variables.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variables : MonoBehaviour {
+//variables 
+	public static int number;  
+	public float speed;
+	public string name;
+
+	// Use this for initialization
+	void Start () {
+		number = 10;
+	/*
+	this
+	is
+	a
+	multiline
+	comment 
+	*/
+		speed = 0.94f;
+		name = "Bob";
+
+		print(name +" is "+ number+"years old"+ speed);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(number,0,0);
+
+	}
+}
